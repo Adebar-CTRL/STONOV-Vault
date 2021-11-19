8c711ca6924cd4d69896fab0b65ddda27dd19de3
ananasadebar <ananasadebar@gmx.de>
2021-11-19 10:30:16 +0100

    classes reminder works now

--- forloop.py
+++ forloop.py
@@ -1,13 +1,10 @@
-from typing_extensions import Self
-
-
 class newCar():
     def __init__(self, price, sell):
         self.carprice = int(price)
         self.sellprice = int(sell)
     def calc(self):
         for i in range(10):
-            self.carprice = self.carprice * 0,8
+            self.carprice = self.carprice * 0.8
             print("YEAR ", i + 1, "Price: ", self.carprice)
             if self.carprice < self.sellprice:
                 print("SEll")
