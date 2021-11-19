fa6244ebb023ca81b4d08996f59a8562f86655c3
ananasadebar <ananasadebar@gmx.de>
2021-11-19 10:24:54 +0100

    added classes

--- forloop.py
+++ forloop.py
@@ -1,7 +1,18 @@
-def runloop(amount):
-    store = 0
-    for i in range(1,amount + 1, 2):
-        store = store + i
-        print(i)
-    print(store)
-runloop(int(input("enter number"))) 
+from typing_extensions import Self
+
+
+class newCar():
+    def __init__(self, price, sell):
+        self.carprice = int(price)
+        self.sellprice = int(sell)
+    def calc(self):
+        for i in range(10):
+            self.carprice = self.carprice * 0,8
+            print("YEAR ", i + 1, "Price: ", self.carprice)
+            if self.carprice < self.sellprice:
+                print("SEll")
+            else:
+                print("hold")
+
+c1 = newCar(input("enter pirce"), input("enter sellprice"))
+c1.calc()
