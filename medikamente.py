396fb35a8a54de22577b79039cd1b6a05bee4ac2
ananasadebar <ananasadebar@gmx.de>
2022-01-31 17:52:48 +0100

    fixed

--- medikamente.py
+++ medikamente.py
@@ -3,7 +3,7 @@
 def mendikamentenmenge(n, dosis):
     n = n - 1
     if n > 0:
-        return mendikamentenmenge(n, dosis * 0.6) 
+        return mendikamentenmenge(n, dosis * 0.6 + dosis) 
     else:
         return dosis
 dosis = mendikamentenmenge(5, 5)
